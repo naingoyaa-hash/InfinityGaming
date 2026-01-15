12d38f1108b1da4a57622dfbafe20ca62f22c4e0
Argenix <jandradep@uteq.edu.ec>
2026-01-14 22:27:46 -0500

    Arge

---
+++ Alerta.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinityGaming
+{
+    internal class Alerta
+    {
+        private int IdAlerta { get; set; }
+        public string Mensaje { get; set; }
+        public double Fecha { get; set; }
+        public string Tipo { get; set; }
+        private bool Leida { get; set; }
+
+        public void GenerarAlarma(string mensaje, string tipo)
+
+        {
+
+        }
+        public void MarcarComoLeida()
+
+        {
+
+        }
+        public void EnviarNotificacion()
+
+        {
+
+        }
+
+    }
+}
